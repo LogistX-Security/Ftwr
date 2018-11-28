9eb221db403cd3b16327c7ef9a2f72b6ae29049e
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-11-28 14:11:10 +0200

    Added basic layout for invite fragment, added some logic for fragment transaction

--- app/src/main/java/com/example/hackintosh/ftwr/fragmnets/InviteFragment.kt
+++ app/src/main/java/com/example/hackintosh/ftwr/fragmnets/InviteFragment.kt
@@ -3,8 +3,21 @@ package com.example.hackintosh.ftwr.fragmnets
 import android.os.Bundle
 import android.view.View
 import com.example.hackintosh.ftwr.R
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TypefaceSpan
+import android.graphics.Typeface
+import android.text.style.RelativeSizeSpan
+import android.widget.Button
+import android.widget.TextView
+
 
 class InviteFragment: BaseFragment() {
+
+    lateinit var inviteMessage: TextView
+    lateinit var callButton: Button
+    lateinit var acceptButton: Button
+    lateinit var rejectButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +27,26 @@ class InviteFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        inviteMessage = view.findViewById(R.id.inviteMessageTextView)
+        callButton = view.findViewById(R.id.callButton)
+        acceptButton = view.findViewById(R.id.acceptButton)
+        rejectButton = view.findViewById(R.id.rejectButton)
+
+        initDetails()
+    }
+
+    fun initDetails() {
+        val actonia = Typeface.createFromAsset(activity?.assets, "fonts/actonia.ttf")
+        val actoniaSpan = TypefaceSpan(actonia)
+
+        val normalBefore = "Would you like to go to a "
+        val custom = "rendezvous"
+        val normalAfter = " with me, and spend a great evening together?"
+        val finalString = normalBefore + custom + normalAfter
+        val sb = SpannableString(finalString)
+        sb.setSpan(actoniaSpan, finalString.indexOf(custom), finalString.indexOf(normalAfter), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        sb.setSpan(RelativeSizeSpan(1.5f), finalString.indexOf(custom), finalString.indexOf(normalAfter), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        inviteMessage.text = sb
     }
 
 }
--- app/src/main/java/com/example/hackintosh/ftwr/fragmnets/QuizFragment.kt
+++ app/src/main/java/com/example/hackintosh/ftwr/fragmnets/QuizFragment.kt
@@ -2,10 +2,14 @@ package com.example.hackintosh.ftwr.fragmnets
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.example.hackintosh.ftwr.R
+import com.example.hackintosh.ftwr.activities.QuestionActivity
 
 
 class QuizFragment: BaseFragment() {
+
+    lateinit var followButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,12 @@ class QuizFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        followButton = view.findViewById(R.id.followButton)
+        followButton.setOnClickListener {moveToInvitePage()}
+    }
+
+    private fun moveToInvitePage() {
+        (activity as QuestionActivity).viewPager.currentItem = 1
     }
 
 }
